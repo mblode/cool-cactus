993e6fc21083ac12ceadf49ecc2816f9507bfc4f
mblode <m.blode@gmail.com>
2015-11-28 16:01:18 +1100

    Fonts and pocket

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,22 +1,8 @@
 //= require jquery
 //= require '_flowtype.js'
-//= require '_imgix.js'
 
 'use strict';
 
 $('.hero__large-title').flowtype({
  fontRatio : 10
 });
-
-var options = {
-			updateOnResizeDown : true,
-			updateOnPinchZoom : true,
-			fitImgTagToContainerWidth: true,
-			fitImgTagToContainerHeight: true,
-			pixelStep : 5,
-      autoInsertCSSBestPractices: true
-	};
-
-	imgix.onready(function() {
-		imgix.fluid(options);
-	});
